e0feed3ec38601a4faffc7bd3830bf140f66d6ee
Matt Johnston <m@ttjohnston.com>
2022-09-29 23:27:52 -0300

    init

---
+++ index.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const core = require('@actions/core');
+const tc = require('@actions/tool-cache');
+const { getDownloadObject } = require('./lib/utils');
+
+async function setup() {
+  try {
+    // Get version of tool to be installed
+    const version = core.getInput('version');
+
+    // Download the specific version of the tool, e.g. as a tarball/zipball
+    const download = getDownloadObject(version);
+    const pathToTarball = await tc.downloadTool(download.url);
+
+    // Extract the tarball/zipball onto host runner
+    const extract = download.url.endsWith('.zip') ? tc.extractZip : tc.extractTar;
+    const pathToCLI = await extract(pathToTarball);
+
+    // Expose the tool by adding it to the PATH
+    core.addPath(path.join(pathToCLI, download.binPath));
+  } catch (e) {
+    core.setFailed(e);
+  }
+}
+
+module.exports = setup
+
+if (require.main === module) {
+  setup();
+}
