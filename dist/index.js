4376daf824323cf21f52da119f8515f72aa9a96a
Matt Johnston <m@ttjohnston.com>
2023-01-18 22:11:42 -0400

    build

--- dist/index.js
+++ dist/index.js
@@ -11,10 +11,14 @@ const tc = __webpack_require__(784);
 async function setup() {
   try {
     // Get version of tool to be installed
-    const version = core.getInput('version');
+    let version = core.getInput('version');
+    if(version === 'main'){
+      version = 'latest'
+    }
     // Download the specific version of the tool, e.g. as a tarball/zipball
-    const pathToBin = await tc.downloadTool(`https://cndi-binaries.s3.amazonaws.com/cndi/${version}/cndi-linux`, `/usr/local/bin/cndi`);
+    const pathToBin = await tc.downloadTool(`https://cndi-binaries.s3.amazonaws.com/cndi/${version}/cndi-linux`, `bin/cndi`);
     core.debug(`Downloaded tool to ${pathToBin}`);
+    
     // Expose the tool by adding it to the PATH
     core.addPath(pathToBin)
     core.debug(`Added ${pathToBin} to PATH`);
