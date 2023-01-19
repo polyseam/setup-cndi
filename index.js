dfcffe9aeae904dc05efebd293d4477c87af4be2
Matt Johnston <m@ttjohnston.com>
2023-01-18 22:06:33 -0400

    use release instead of main by default

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ const tc = require('@actions/tool-cache');
 async function setup() {
   try {
     // Get version of tool to be installed
-    const version = core.getInput('version');
+    let version = core.getInput('version');
+    if(version === 'main'){
+      version = 'latest'
+    }
     // Download the specific version of the tool, e.g. as a tarball/zipball
     const pathToBin = await tc.downloadTool(`https://cndi-binaries.s3.amazonaws.com/cndi/${version}/cndi-linux`, `/usr/local/bin/cndi`);
     core.debug(`Downloaded tool to ${pathToBin}`);
+    
     // Expose the tool by adding it to the PATH
     core.addPath(pathToBin)
     core.debug(`Added ${pathToBin} to PATH`);
