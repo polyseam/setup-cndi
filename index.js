7bff1b47a5e5a7636affd9fa643dbc17e0570642
Matt Johnston <m@ttjohnston.com>
2024-02-02 21:33:14 -0400

    extractTar

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
-const core = require("@actions/core");
-const tc = require("@actions/tool-cache");
-const exec = require("@actions/exec");
+const core = require('@actions/core');
+const tc = require('@actions/tool-cache');
+const exec = require('@actions/exec');
 
 async function setup() {
   try {
     // Get version of tool to be installed
-    let version = core.getInput("version") || "latest";
+    let version = core.getInput('version') || 'latest';
     let downloadUrl = `https://github.com/polyseam/cndi/releases/${version}/download/cndi-linux.tar.gz`;
 
-    if (version === "main") {
+    if(version === 'main'){
       downloadUrl = `https://cndi-binaries.s3.amazonaws.com/cndi/main/cndi-linux.tar.gz`;
     }
 
     // Download the specific version of the tool, e.g. as a tarball/zipball
-    const pathToBin = await tc.downloadTool(downloadUrl, "/usr/local/bin/cndi");
+    const pathToTar = await tc.downloadTool(downloadUrl, "/usr/local/bin/");
+    const pathToBin = await tc.extractTar(pathToTar);
     core.debug(`Downloaded tool to ${pathToBin}`);
-    core.addPath(pathToBin);
+    core.addPath(pathToBin)
     core.debug(`Added ${pathToBin} to PATH`);
-    exec.exec("chmod +x /usr/local/bin/cndi");
-    core.debug("Set cndi to be executable");
+    exec.exec(`chmod +x ${pathToBin}`);
+    core.debug('Set cndi to be executable');
   } catch (e) {
     core.setFailed(e);
   }
 }
 
-module.exports = setup;
+module.exports = setup
 
 if (require.main === module) {
   setup();
